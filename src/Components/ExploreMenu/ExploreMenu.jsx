116625dd74f84269c3cac7015d48516ecb691a3f
Sandhiya1903 <sandhiyaelumalai19@gmail.com>
2024-09-22 20:46:56 +0530

    Cart-set

--- src/Components/ExploreMenu/ExploreMenu.jsx
+++ src/Components/ExploreMenu/ExploreMenu.jsx
@@ -1,25 +1,39 @@
-import React from 'react'
-import './ExploreMenu.css'
-import {menu_list} from '../../assets/assets'
-const ExploreMenu = ({category,setCategory}) => {
+import React from "react";
+import "./ExploreMenu.css";
+import { menu_list } from "../../assets/assets";
+const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className='explore-menu' id="explore-menu">
+    <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
-      <p className='explore-menu-text'>Discover a variety of delectable dishes crafted with the finest ingredients. Dive into our menu to experience culinary excellence and satisfy your taste buds.</p>
+      <p className="explore-menu-text">
+        Discover a variety of delectable dishes crafted with the finest
+        ingredients. Dive into our menu to experience culinary excellence and
+        satisfy your taste buds.
+      </p>
       <div className="explore-menu-list">
-       {menu_list.map((item,index)=>{
-        return(
-            <div onClick={()=>setCategory(prev=>prev===item.menu_name?"All":item.menu_name)} key={index}className='explore-menu-list-item'>
-                <img className={category===item.menu_name?"active":""}src={item.menu_image}/>
-                <p>{item.menu_name}</p>
-                            </div>
-        )
-       })}
+        {menu_list.map((item, index) => {
+          return (
+            <div
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+              />
+              <p>{item.menu_name}</p>
+            </div>
+          );
+        })}
       </div>
-      <hr/>
+      <hr />
     </div>
+  );
+};
 
-  )
-}
-
-export default ExploreMenu
+export default ExploreMenu;
